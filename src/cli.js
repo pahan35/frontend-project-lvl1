7524ce62a1b935a6c34f985a0f9a510cff48b75d
Pavlo Zhukov <pahazhukov@gmail.com>
2020-06-07 04:54:41 +0300

    feat: even game

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,12 @@
 import readlineSync from 'readline-sync';
 
-export default function cli() {
+export function askName() {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+  return name;
+}
+
+export default function cli() {
+  askName();
 }
